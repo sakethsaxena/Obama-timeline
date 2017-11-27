c57bb660dd4f974dc7ed2fe0d179ef76b9617f00
DESKTOP-FA13DJJ\deshm <shantanuspark@gmail.com>
2017-11-27 00:54:28 -0800

    added timeline and fixed selection issue

--- public/javascripts/timelineApp.js
+++ public/javascripts/timelineApp.js
@@ -18,14 +18,26 @@ app.config(function($routeProvider){
 });
 
 app.factory('postService', function($resource){
-	return $resource('/data/posts/twp/:selectedYear');
+	return $resource('/data/posts/:type/:selectedYear/:selectedMonth');
 });
 
 app.controller('timelineController', function(postService, $scope, $rootScope){
     $scope.years = [2016, 2015, 2014, 2013, 2012, 2011, 2010, 2009, 2008];
     $scope.months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
     $scope.selectedYear = 2016;
-    $scope.selectedMonth =  'Jan';
-
-    $scope.posts = postService.query({selectedYear: $scope.selectedYear});
+	$scope.selectedMonth =  'Jan';
+	$scope.postsType = 'all';
+	
+	$scope.posts = postService.query({type: $scope.postsType, selectedYear: $scope.selectedYear, selectedMonth:$scope.months.indexOf($scope.selectedMonth) + 1});
+	
+	$scope.data = {
+		getResultsForYear : function(year) {
+			$scope.selectedYear = year;
+			$scope.posts = postService.query({type: $scope.postsType, selectedYear: year})
+		},
+		getResultsForMonth : function(month) {
+			$scope.selectedMonth = month;
+			$scope.posts = postService.query({type: $scope.postsType, selectedYear: $scope.selectedYear, selectedMonth:$scope.months.indexOf(month) + 1})
+		}
+	}
 });
--- routes/api.js
+++ routes/api.js
@@ -1,52 +1,73 @@
 var express = require('express');
 var router = express.Router();
-var mongoose = require( 'mongoose' );
+var mongoose = require('mongoose');
 var Posts = mongoose.model('Posts');
 
 router.route('/posts/')
-//gets all posts
-  .get(function(req, res){
-    Posts.find(function(err, post){
-      if(err)
+  //gets all posts
+  .get(function (req, res) {
+    Posts.find(function (err, post) {
+      if (err)
         res.send(err);
       res.json(post);
     });
-  }); 
+  });
 
-  router.route('/posts/:type/:year')
+router.route('/posts/:type/:year')
   //gets posts of specified type in a year
-    .get(function(req, res){
-        query = {
-          "type": req.params.type,
-          "timestamp": {
-              "$gte": new Date(parseInt(req.params.year) - 1, 12, 01),
-              "$lte": new Date(req.params.year, 11, 31)
-          }
-        };
-        console.log(query);
-        Posts.find(query, function(err, post){
-            if(err)
-                res.send(err);
-          res.json(post);
-        })
-    }); 
 
-  router.route('/posts/:type/:year/:month')
+  .get(function (req, res) {
+    query = "";
+    if (req.params.type == 'all') {
+      query = {
+        "timestamp": {
+          "$gte": new Date(parseInt(req.params.year) - 1, 12, 01),
+          "$lte": new Date(req.params.year, 11, 31)
+        }
+      }
+    } else {
+      query = {
+        "type": req.params.type,
+        "timestamp": {
+          "$gte": new Date(parseInt(req.params.year) - 1, 12, 01),
+          "$lte": new Date(req.params.year, 11, 31)
+        }
+      }
+    }
+    console.log(query);
+    Posts.find(query, function (err, post) {
+      if (err)
+        res.send(err);
+      res.json(post);
+    })
+  });
+
+router.route('/posts/:type/:year/:month')
   //get posts of specified type in a year in a month
-    .get(function(req, res){
-        query = {
-          "type": req.params.type,
-          "timestamp": {
-              "$gte": new Date(req.params.year, parseInt(req.params.month) - 1, 01),
-              "$lt": new Date(req.params.year, parseInt(req.params.month), 01)
-          }
-        };
-        console.log(query);
-        Posts.find(query, function(err, post){
-            if(err)
-                res.json([]);
-          res.json(post);
-        })
-    });
+  .get(function (req, res) {
+    query = "";
+    if (req.params.type == 'all') {
+      query = {
+        "timestamp": {
+          "$gte": new Date(req.params.year, parseInt(req.params.month) - 1, 01),
+          "$lt": new Date(req.params.year, parseInt(req.params.month), 01)
+        }
+      }
+    } else {
+      query = {
+        "type": req.params.type,
+        "timestamp": {
+          "$gte": new Date(req.params.year, parseInt(req.params.month) - 1, 01),
+          "$lt": new Date(req.params.year, parseInt(req.params.month), 01)
+        }
+      }
+    }
+    console.log(query);
+    Posts.find(query, function (err, post) {
+      if (err)
+        res.json([]);
+      res.json(post);
+    })
+  });
 
 module.exports = router;
